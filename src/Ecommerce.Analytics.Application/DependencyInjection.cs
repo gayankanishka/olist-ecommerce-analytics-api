82773545bf3e5fb6e2d8f6b04d94da8aa22f6235
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-07 19:09:35 +0530

    feat: add initial solution structure

---
+++ src/Ecommerce.Analytics.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Analytics.Application
+{
+    /// <summary>
+    /// Dependency injection extension to configure Application layer services.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Configure Application layer services.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/Ecommerce.Analytics.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Analytics.Infrastructure
+{
+    /// <summary>
+    /// Dependency injection extension to configure Infrastructure layer services.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Configure Infrastructure layer services.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
