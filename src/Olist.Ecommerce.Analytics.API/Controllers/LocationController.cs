d1bf1b982fc0ae61adcfc1ba35a920cd56c2cc38
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-21 23:19:17 +0530

    feat: add location controller

---
+++ src/Olist.Ecommerce.Analytics.API/Controllers/LocationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Olist.Ecommerce.Analytics.API.Controllers
+{
+    [Route("api/locations")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetLocationsAsync([FromQuery] string limit = "100")
+        {
+            // TODO: This should return all location objects
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("most-profitable")]
+        public async Task<IActionResult> GetMostProfitableLocationsAsync()
+        {
+            // TODO: This should return a list of most profitable location objects
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("least-profitable-locations")]
+        public async Task<IActionResult> GetLeastProfitableLocationsAsync()
+        {
+            // TODO: This should be a list of location objects with the item object
+
+            return Ok();
+        }
+    }
+}
