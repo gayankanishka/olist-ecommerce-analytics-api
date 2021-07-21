dfbd64e0b855fc1f24394077fbd5b7ac8033436e
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-21 23:19:00 +0530

    feat: add seller controller

---
+++ src/Olist.Ecommerce.Analytics.API/Controllers/SellerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Olist.Ecommerce.Analytics.API.Controllers
+{
+    [Route("api/sellers")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("most-popular/{itemId}")]
+        public async Task<IActionResult> GetMostPopularSellersAsync([FromRoute] string itemId)
+        {
+            // TODO: This should return the most popular seller objects by itemId
+
+            return Ok();
+        }
+    }
+}
