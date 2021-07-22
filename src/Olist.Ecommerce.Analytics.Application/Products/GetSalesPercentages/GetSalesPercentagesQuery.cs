3f276192a155c7752d3c07c636e7b7bd7f9ff852
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-22 20:11:01 +0530

    feat: add `SalesPercentages` query and handler

---
+++ src/Olist.Ecommerce.Analytics.Application/Products/GetSalesPercentages/GetSalesPercentagesQuery.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Olist.Ecommerce.Analytics.Domain.Enums;
+using Olist.Ecommerce.Analytics.Domain.Models;
+
+namespace Olist.Ecommerce.Analytics.Application.Products.GetSalesPercentages
+{
+    public class GetSalesPercentagesQuery : IRequest<IEnumerable<SalesPercentage>>
+    {
+        public DateFilters Filter { get; set; }
+
+        public GetSalesPercentagesQuery()
+        {
+        }
+
+        public GetSalesPercentagesQuery(DateFilters filter)
+        {
+            Filter = filter;
+        }
+    }
+}
---
+++ src/Olist.Ecommerce.Analytics.Application/Products/GetSalesPercentages/GetSalesPercentagesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Olist.Ecommerce.Analytics.Domain.Enums;
+using Olist.Ecommerce.Analytics.Domain.Models;
+
+namespace Olist.Ecommerce.Analytics.Application.Products.GetSalesPercentages
+{
+    public class GetSalesPercentagesQueryHandler :
+        IRequestHandler<GetSalesPercentagesQuery, IEnumerable<SalesPercentage>>
+    {
+        public Task<IEnumerable<SalesPercentage>> Handle(GetSalesPercentagesQuery request,
+            CancellationToken cancellationToken)
+        {
+            if (request.Filter == DateFilters.None)
+            {
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
