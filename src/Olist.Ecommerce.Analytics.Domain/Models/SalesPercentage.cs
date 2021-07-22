672d87fed5ecd0c1b72de24428c77475a178171c
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-22 20:08:23 +0530

    feat: add sales percentages model

---
+++ src/Olist.Ecommerce.Analytics.Domain/Models/SalesPercentage.cs
@@ -0,0 +1,8 @@
+﻿namespace Olist.Ecommerce.Analytics.Domain.Models
+{
+    public class SalesPercentage
+    {
+        public Product Product { get; set; }
+        public double Value { get; set; }
+    }
+}
