ddb569ff7b9eb0db13f8678a05808b4e11195e22
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-22 20:47:37 +0530

    docs: add products API documentation

--- src/Olist.Ecommerce.Analytics.API/Controllers/ProductController.cs
+++ src/Olist.Ecommerce.Analytics.API/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Net;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Olist.Ecommerce.Analytics.Application.Products.GetFastMovingProducts;
 using Olist.Ecommerce.Analytics.Application.Products.GetSalesPercentages;
@@ -11,39 +11,59 @@ using Olist.Ecommerce.Analytics.Domain.Models;
 
 namespace Olist.Ecommerce.Analytics.API.Controllers
 {
+    /// <summary>
+    /// Handles Products related analytics operations.
+    /// </summary>
     [Route("api/products")]
     [ApiController]
     public class ProductController : ControllerBase
     {
         private readonly IMediator _mediator;
 
+        /// <summary>
+        /// Constructor of ProductsController.
+        /// </summary>
+        /// <param name="mediator"></param>
         public ProductController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Get fast selling products.
+        /// </summary>
+        /// <returns>A list of fast selling products.</returns>
+        /// <response code="200">Fast selling products list.</response>
+        /// <response code="400">If invalid payload is passed</response>
+        /// <response code="500">If something went wrong in the server-end</response>
         [Route("fast-moving")]
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetFastMovingProductsAsync()
         {
-            // TODO: This should return top selling products
-            IEnumerable<Product> products = await _mediator.Send(new GetFastMovingProductsQuery());
+            IEnumerable<Product> products = await _mediator.Send(
+                new GetFastMovingProductsQuery());
 
             return Ok(products);
         }
 
+        /// <summary>
+        /// Get slow selling products.
+        /// </summary>
+        /// <param name="locationId">The ID of the seller location.</param>
+        /// <returns>A list of slow selling products along with users.</returns>
+        /// <response code="200">Slow selling products list.</response>
+        /// <response code="400">If invalid payload is passed.</response>
+        /// <response code="500">If something went wrong in the server-end.</response>
         [Route("{locationId}/slow-moving")]
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetSlowMovingProductsAsync([FromRoute] string locationId)
         {
-            // TODO: This should return the slow moving items as objects and users as objects alongside
-
             if (string.IsNullOrWhiteSpace(locationId))
             {
                 return BadRequest("LocationId required!");
@@ -54,16 +74,22 @@ namespace Olist.Ecommerce.Analytics.API.Controllers
 
             return Ok(products);
         }
-
+        
+        /// <summary>
+        /// Get sales percentages.
+        /// </summary>
+        /// <param name="filter">The <see cref="DateFilters"/> used to get relevant data.</param>
+        /// <returns>A list of categories with sales percentages.</returns>
+        /// <response code="200">Sales percentages list.</response>
+        /// <response code="400">If invalid payload is passed.</response>
+        /// <response code="500">If something went wrong in the server-end.</response>
         [Route("sales-percentages")]
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<SalesPercentage>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(IEnumerable<SalesPercentage>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetSalesPercentagesAsync([FromQuery] DateFilters filter)
         {
-            // TODO: This should return a list of categories with percentages
-
             IEnumerable<SalesPercentage> salesPercentages = await _mediator.Send(
                 new GetSalesPercentagesQuery(filter));
 
