087fa763de6324899d1a964566ff894e68559607
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-22 16:35:08 +0530

    feat: hdfs const operations

---
+++ src/Olist.Ecommerce.Analytics.Infrastructure/Hadoop/HdfsOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace Olist.Ecommerce.Analytics.Infrastructure.Hadoop
+{
+    public class HdfsOperations
+    {
+        public const string OPEN_AND_READ = "OPEN";
+        public const string FILE_OR_DIRECTORY_STATUS = "GETFILESTATUS";
+        public const string LIST_DIRECTORY = "LISTSTATUS";
+    }
+}
