c36d33e20321cb1fe3cc6fad8995cbd5edc21ff1
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-22 20:10:37 +0530

    feat: add `SlowMoving` query and handler

---
+++ src/Olist.Ecommerce.Analytics.Application/Products/GetSlowMovingProducts/GetSlowMovingProductsQuery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Olist.Ecommerce.Analytics.Domain.Models;
+
+namespace Olist.Ecommerce.Analytics.Application.Products.GetSlowMovingProducts
+{
+    public class GetSlowMovingProductsQuery : IRequest<IEnumerable<Product>>
+    {
+        public string LocationId { get; }
+
+        public GetSlowMovingProductsQuery(string locationId)
+        {
+            LocationId = locationId;
+        }
+    }
+}
---
+++ src/Olist.Ecommerce.Analytics.Application/Products/GetSlowMovingProducts/GetSlowMovingProductsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Olist.Ecommerce.Analytics.Domain.Models;
+
+namespace Olist.Ecommerce.Analytics.Application.Products.GetSlowMovingProducts
+{
+    public class GetSlowMovingProductsQueryHandler :
+        IRequestHandler<GetSlowMovingProductsQuery, IEnumerable<Product>>
+    {
+        public Task<IEnumerable<Product>> Handle(GetSlowMovingProductsQuery request,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
