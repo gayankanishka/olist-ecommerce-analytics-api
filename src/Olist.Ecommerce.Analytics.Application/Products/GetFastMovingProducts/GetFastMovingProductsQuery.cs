98b16298fa1f00a5c7132007bcc93c7047491e56
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-22 20:10:21 +0530

    feat: add `FastMovingProducts` query and handler

---
+++ src/Olist.Ecommerce.Analytics.Application/Products/GetFastMovingProducts/GetFastMovingProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Olist.Ecommerce.Analytics.Domain.Models;
+
+namespace Olist.Ecommerce.Analytics.Application.Products.GetFastMovingProducts
+{
+    public class GetFastMovingProductsQuery : IRequest<IEnumerable<Product>>
+    {
+    }
+}
---
+++ src/Olist.Ecommerce.Analytics.Application/Products/GetFastMovingProducts/GetFastMovingProductsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Olist.Ecommerce.Analytics.Domain.Models;
+
+namespace Olist.Ecommerce.Analytics.Application.Products.GetFastMovingProducts
+{
+    public class GetFastMovingProductsQueryHandler :
+        IRequestHandler<GetFastMovingProductsQuery, IEnumerable<Product>>
+    {
+        public async Task<IEnumerable<Product>> Handle(GetFastMovingProductsQuery request,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
