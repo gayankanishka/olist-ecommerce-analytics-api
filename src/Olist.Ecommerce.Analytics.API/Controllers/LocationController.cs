f2460b36731ecac06bdd21e37a15a2d0da0ae5c2
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-07-23 12:07:42 +0530

    feat: add memory and response caching in locations controller

--- src/Olist.Ecommerce.Analytics.API/Controllers/LocationController.cs
+++ src/Olist.Ecommerce.Analytics.API/Controllers/LocationController.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Olist.Ecommerce.Analytics.Application.Locations.GetMostRevenueLocations;
+using Olist.Ecommerce.Analytics.Domain.Constants;
 using Olist.Ecommerce.Analytics.Domain.Models;
 
 namespace Olist.Ecommerce.Analytics.API.Controllers
@@ -16,14 +19,16 @@ namespace Olist.Ecommerce.Analytics.API.Controllers
     public class LocationController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly IMemoryCache _memoryCache;
 
         /// <summary>
         /// Constructor of LocationController.
         /// </summary>
         /// <param name="mediator"></param>
-        public LocationController(IMediator mediator)
+        public LocationController(IMediator mediator, IMemoryCache memoryCache)
         {
             _mediator = mediator;
+            _memoryCache = memoryCache;
         }
 
         /// <summary>
@@ -34,14 +39,28 @@ namespace Olist.Ecommerce.Analytics.API.Controllers
         /// <response code="400">If invalid payload is passed</response>
         /// <response code="500">If something went wrong in the server-end</response>
         [Route("most-revenue")]
+        [ResponseCache(Duration = 600)]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Location>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetMostRevenueLocationsAsync()
         {
-            IEnumerable<Location> locations = await _mediator.Send(
-                new GetMostRevenueLocationsQuery());
+            if (_memoryCache.TryGetValue(CacheKeys.MostRevenueLocations, out IEnumerable<Location> locations))
+            {
+                return Ok(locations);
+            }
+
+            locations = await _mediator.Send(new GetMostRevenueLocationsQuery());
+
+            MemoryCacheEntryOptions cacheExpiryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.UtcNow.AddMinutes(60),
+                Priority = CacheItemPriority.High,
+                SlidingExpiration = TimeSpan.FromMinutes(30)
+            };
+
+            _memoryCache.Set(CacheKeys.MostRevenueLocations, locations, cacheExpiryOptions);
 
             return Ok(locations);
         }
